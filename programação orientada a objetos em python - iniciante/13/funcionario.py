e10953d99fa9df9ebbaa321e0fbfb66c7a1095fd
Bianca Narciso <100371067+bian-nca@users.noreply.github.com>
2022-06-25 22:31:35 -0300

    Delete programação orientada a objetos em python - iniciante directory

--- programação orientada a objetos em python - iniciante/13/funcionario.py
+++
@@ -1,9 +0,0 @@
-class Funcionario:
-    def __init__(self, nome, salario):
-        self.nome = nome
-        self.salario = salario
-
-    def status(self):
-        print("------------------------------FUNCIONARIO-----------------------")
-        print(f"Nome do funcionario: {self.nome}")
-        print(f"Salario do(a) {self.nome}: R$ {self.salario} reais")
--- programação orientada a objetos em python - iniciante/13/main.py
+++
@@ -1,7 +0,0 @@
-from funcionario import Funcionario
-
-f1 = Funcionario('Bianca', 1.212)
-f1.status()
-
-f2 = Funcionario('Leonan', 4500.00)
-f2.status()
